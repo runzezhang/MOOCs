b930281fe15d7bb34af4b5bffbccc4de57c6672b
runzezhang <runze.zhang@outlook.com>
2019-05-20 00:59:13 -0500

    update footer and header

--- Coursera/Front-End Web Development with React/confusion/src/App.js
+++ Coursera/Front-End Web Development with React/confusion/src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
--- Coursera/Front-End Web Development with React/confusion/src/components/MainComponent.js
+++ Coursera/Front-End Web Development with React/confusion/src/components/MainComponent.js
@@ -5,6 +5,8 @@ import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 class Main extends Component {
 
   constructor(props) {
@@ -19,10 +21,21 @@ class Main extends Component {
     this.setState({ selectedDish: dishId});
   }
 
-  render() {
+    render() {
+        const HomePage = () => {
+            return(
+                <Home 
+                />
+            );
+          }
     return (
       <div>
         <Header />
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+        </Switch>
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         <Footer />
