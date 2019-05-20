847d2b69b1b0d0cf6a59c3921d9e5786430fd7a6
runzezhang <runze.zhang@outlook.com>
2019-05-20 01:05:25 -0500

    update SPA 1

--- Coursera/Front-End Web Development with React/confusion/src/components/MainComponent.js
+++ Coursera/Front-End Web Development with React/confusion/src/components/MainComponent.js
@@ -7,15 +7,22 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          dishes: DISHES,
+          comments: COMMENTS,
+          promotions: PROMOTIONS,
+          leaders: LEADERS
+        };
+      }
 
   onDishSelect(dishId) {
     this.setState({ selectedDish: dishId});
@@ -25,15 +32,20 @@ class Main extends Component {
         const HomePage = () => {
             return(
                 <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
           }
+          
     return (
       <div>
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
         </Switch>
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
