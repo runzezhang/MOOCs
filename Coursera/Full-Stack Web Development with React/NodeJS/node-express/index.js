8a539d3ecbdafc9cd064cee97d966dcaf748fe02
Runze Zhang <ramsey.zhang@outlook.com>
2019-07-02 17:46:50 -0500

    update express API dishes

--- Coursera/Full-Stack Web Development with React/NodeJS/node-express/index.js
+++ Coursera/Full-Stack Web Development with React/NodeJS/node-express/index.js
@@ -1,13 +1,23 @@
-const express = require('express'),
-     http = require('http');
+const express = require('express');
+http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+// const promoRouter = require('./routes/promoRouter');
+// const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+// app.use('/promotions', promoRouter);
+// app.use('/leaders', leaderRouter);
 app.use(express.static(__dirname + '/public'));
+
 app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
