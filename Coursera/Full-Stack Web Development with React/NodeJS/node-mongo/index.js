f1ee8be9e0dc856c4ec5f954f3e217ef9a4edd41
Runze Zhang <ramsey.zhang@outlook.com>
2019-07-07 17:02:23 -0500

    update

--- Coursera/Full-Stack Web Development with React/NodeJS/node-mongo/index.js
+++ Coursera/Full-Stack Web Development with React/NodeJS/node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -11,28 +12,34 @@ MongoClient.connect(url, (err, client) => {
   console.log('Connected correctly to server');
 
   const db = client.db(dbname);
-  const collection = db.collection("dishes");
-  collection.insertOne({
-      "name": "Uthappizza",
-      "description": "test"
+  dboper.insertDocument(db, {
+      name: "Vadonut",
+      description: "Test"
     },
-    (err, result) => {
-      assert.equal(err, null);
+    "dishes", (result) => {
+      console.log("Insert Document:\n", result.ops);
 
-      console.log("After Insert:\n");
-      console.log(result.ops);
+      dboper.findDocuments(db, "dishes", (docs) => {
+        console.log("Found Documents:\n", docs);
 
-      collection.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
+        dboper.updateDocument(db, {
+            name: "Vadonut"
+          }, {
+            description: "Updated Test"
+          }, "dishes",
+          (result) => {
+            console.log("Updated Document:\n", result.result);
 
-        console.log("Found:\n");
-        console.log(docs);
+            dboper.findDocuments(db, "dishes", (docs) => {
+              console.log("Found Updated Documents:\n", docs);
 
-        db.dropCollection("dishes", (err, result) => {
-          assert.equal(err, null);
+              db.dropCollection("dishes", (result) => {
+                console.log("Dropped Collection: ", result);
 
-          client.close();
-        });
+                client.close();
+              });
+            });
+          });
       });
     });
 
