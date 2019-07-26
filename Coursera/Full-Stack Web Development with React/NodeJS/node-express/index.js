4a681dc5107af13939c79054b2c6fe5af676e673
runzezhang <runze.zhang@outlook.com>
2019-07-26 01:31:14 -0500

    update

--- Coursera/Full-Stack Web Development with React/NodeJS/node-express/index.js
+++ Coursera/Full-Stack Web Development with React/NodeJS/node-express/index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
