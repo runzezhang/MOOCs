e75e81571b85f0819e560320de44e6663e9ff186
Runze Zhang <ramsey.zhang@outlook.com>
2019-11-15 15:52:25 -0600

    update Vue Class

--- Udemy/REST-Django-VueJS/C4/ebookapi/ebooks/models.py
+++ Udemy/REST-Django-VueJS/C4/ebookapi/ebooks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Ebook(models.Modal):
+  title = models.CharField(max_length = 140)
+  author = models.CharField(max_length = 60)
+  description = models.TextField()
+  publication_date = models.DateField()
+
+  def __str__(self):
+    return self.title
+
+class Review(models.Model):
+  created_at = models.DateTimeField(auto_now_add = True)
+  update_at = models.DateTimeField(auto_now=True)
+  review_author = models.CharField(max)
